8940dcc54691576e4ed4178df3f8cfb06a0b376b
pd11256 <61539626+pd11256@users.noreply.github.com>
2020-04-14 10:44:07 +0900

    집 --> 커밋

--- a002_layout/src/main/java/com/lec/android/a002_layout/MainActivity.java
+++ a002_layout/src/main/java/com/lec/android/a002_layout/MainActivity.java
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.linear1);
+
+        int ss = 23;
+        String tt ="하하";
     }
 }
